f74039cd95672c92a00dcc72cc9737665dc5be95
FreeVic <freeverse@qq.com>
2018-01-11 20:07:43 +0800

    添加 Image使用

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import { AppRegistry } from 'react-native';
 // import App1 from './App1';
 // import App from './Navigator'
 // import App from './App'
-import TestText from './TestText'
-
-AppRegistry.registerComponent('testapp', () => TestText);
+// import TestText from './src/TestText'
+import TestImage from './src/TestImage'
+AppRegistry.registerComponent('testapp', () => TestImage);
---
+++ src/TestImage.js
@@ -0,0 +1,67 @@
+/**
+ * Created by zhangshengli on 2018/1/11.
+ */
+import React,{Component} from 'react';
+import {
+    View,
+    Image,
+    StyleSheet,
+    Alert,
+} from 'react-native';
+
+export default class TestImage extends Component{
+    constructor(props){
+        super(props)
+
+    }
+
+    mOnLoad(){
+        console.log('mOnLoad')
+    }
+
+    mOnLayout(){
+        console.log('onLayout')
+    }
+
+    mOnLoadStart(){
+        console.log('onLoadStart')
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+            <Image
+                style={styles.img}
+                // source={{uri:'../img/1.png'}}
+                source={require('../img/1.png')}
+                blurRadius={1}
+            />
+
+            <Image
+                style={styles.img}
+                onload={()=>this.mOnLoad()}
+                onLayout={()=>this.mOnLayout()}
+                onLoadStart={()=>this.mOnLoadStart()}
+                resizeMode={'contain'} // enum('cover', 'contain', 'stretch', 'repeat', 'center')
+                source={{uri: 'https://reactnative.cn/static/docs/0.51/img/components/image.png'}}
+            />
+            </View>
+        )
+    }
+}
+
+const styles= StyleSheet.create({
+    container:{
+        flexDirection:'column'
+    },
+    img:{
+        width:200,
+        height:200,
+        borderColor:"#707070",
+        borderRadius:14,
+        borderWidth:2,
+        borderBottomLeftRadius:2,
+        borderBottomRightRadius:14,
+        overlayColor:"#FFFFFF",
+    }
+});
