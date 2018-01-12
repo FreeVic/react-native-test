9205148a33d2f6804cecea8b55d75b87904cbb68
FreeVic <freeverse@qq.com>
2018-01-12 16:10:32 +0800

    添加状态值回调

---
+++ src/TestBase.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zhangshengli on 2018/1/11.
+ */
+import React,{Component} from 'react';
+import {
+   View,
+    Alert,
+} from 'react-native';
+
+export default class TestBase extends Component{
+
+    render(){
+
+        return(
+            <View>
+
+            </View>
+        )
+    }
+}
+
+const styles= StyleSheet.create({
+
+});
--- src/TestTouchable.js
+++ src/TestTouchable.js
@@ -3,15 +3,32 @@
  */
 import React,{Component} from 'react';
 import {
+    View,
     Text,
     ScrollView,
     StyleSheet,
     Image,
     Button,
     Alert,
+    Slider,
 } from 'react-native';
 
 export default class TestTouchable extends Component{
+    constructor(props){
+        super(props)
+
+        this.props={
+            value:12,
+        }
+
+        this.state = {
+            value:this.props.value,
+        }
+
+
+    }
+
+
     mTouch(){
         console.log('touch')
     }
@@ -22,9 +39,20 @@ export default class TestTouchable extends Component{
             Alert.alert("按钮点击事件")
         }
 
+        let mOnSlideComplete=(value)=>{
+
+            Alert.alert('mOnSlideComplete')
+        }
+
+        let mOnSliding = (value)=>{
+            this.setState({value:value})
+            console.log('mOnSliding')
+        }
+
+
         return(
             <ScrollView
-                contentContainerStyle={{flex: 1,justifyContent:'flex-start',top:20}}
+                contentContainerStyle={styles.container}
                 maximumZoomScale={3}
                 minimumZoomScale={1}>
                 {/*<Text style={styles.txt} onpress={()=>this.mTouch()}>这是个文本</Text>*/}
@@ -36,6 +64,20 @@ export default class TestTouchable extends Component{
                     title={'按钮'}
                     onPress={buttonPress}
                 ></Button>
+                <Slider
+                    maximumTrackTintColor='#FF0000'
+                    minimumTrackTintColor='#00FF00'
+                    maximumValue={20}
+                    minimumValue={10}
+                    onSlidingComplete={mOnSlideComplete}
+                    onValueChange={mOnSliding}
+                    step={2}
+                    // thumbImage={require('../img/1.png')}
+                    thumbTintColor={'#0000FF'}
+                    value={this.state.value}
+                >
+                </Slider>
+                <Text>当前值：{this.state.value}</Text>
             </ScrollView>
         )
     }
@@ -43,9 +85,10 @@ export default class TestTouchable extends Component{
 
 const styles= StyleSheet.create({
     container:{
+        top:20,
         flexDirection:'column',
         justifyContent:'flex-start',
-        alignItems:'flex-start',
+        // alignItems:'flex-start',
     },
     txt:{
         fontSize:30,
